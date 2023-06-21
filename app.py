4fc232bce9361200caed57d792a826fa1ed0de12
jholmes16 <Jahmaul.Holmes@marist.edu>
2023-06-21 12:35:16 -0400

    Add templates and connect css to templates

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-from flask import flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+@app.route('/add_food')
+def add_food():
+    return render_template('add_food.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
